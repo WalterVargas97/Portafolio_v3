719a52c513e2a6e1b1cba126340842d1ea507969
wally1997 <walterbolivar97@gmail.com>
2023-02-17 22:12:02 -0500

    changes in profile photo, download cv, whatsapp, and create other part for projects and experience, just need add in admin and create django for

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import project
+from .models import experience
 
 # Register your models here.
+
+admin.site.register(project)
+admin.site.register(experience)
